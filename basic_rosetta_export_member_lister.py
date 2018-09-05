67fde8337707a22fda9a523dcb1f4a07309284cb
jayGattusoNLNZ <jay.gattuso@dia.govt.nz>
2018-09-06 10:36:28 +1200

    clean up

--- basic_rosetta_export_member_lister.py
+++ basic_rosetta_export_member_lister.py
@@ -1,18 +1,10 @@
 import os
 import shutil
 import tarfile
-import pickle
 
 ##### edit these two lines only #####
 source = r"K:\cartes_de_visites"
-destination = r"K:\cartes_de_visites_unpacked"
 #####################################
-
-if not os.path.exists(destination):
-	os.makedirs(destination)
-
-data = []
-files_list = os.listdir(source)
 for item in os.listdir(source):
 	item_path = os.path.join(source, item)
 	try:
@@ -22,10 +14,14 @@ for item in os.listdir(source):
 	for rep in os.listdir(item_path):
 		for my_file in os.listdir(os.path.join(item_path, rep)):
 			my_file_path = os.path.join(item_path, rep, my_file)
-			tar = tarfile.open(my_file_path, "r:")
-			files = tar.getnames()
-			for f in files:
-				with open("files_in_tar.txt", "a") as data:
-					print (my_file_path)
-					print (f)
-					data.write(("{}|{}\n").format(my_file_path, f))
+			try:
+				tar = tarfile.open(my_file_path, "r:")
+				files = tar.getnames()
+				for f in files:
+					with open("files_in_tar.txt", "a") as data:
+						print (my_file_path)
+						print (f)
+						data.write(("{}|{}\n").format(my_file_path, f))
+			except tarfile.ReadError:
+				with open("missed_tars_from_membership_count.txt", "a") as data:
+					data.write("{}\n".format(my_file_path))
